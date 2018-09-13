d84e2908c4b233b85fe88188e61f9e2b6af5bbcd
WinHong\liwh <liwh@winhong.com>
2018-09-13 13:38:10 +0800

    测试springboot

---
+++ lwh-springboot-sqlite-mybatis-example/src/test/java/com/lwhtarena/sqlite/SpringbootApplicationTests.java
@@ -0,0 +1,28 @@
+package com.lwhtarena.sqlite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <p>
+ * <h2>简述：</h2>
+ * <ol></ol>
+ * <h2>功能描述：</h2>
+ * <ol></ol>
+ * </p>
+ *
+ * @Author: liwh
+ * @Date :
+ * @Version: 版本
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("hello world");
+    }
+}
