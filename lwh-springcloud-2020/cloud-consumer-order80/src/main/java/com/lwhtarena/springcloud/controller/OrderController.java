9111aeda6ff808366b3a8fe156846643edd27338
liwh <lwhtarena@163.com>
2020-05-04 12:25:52 +0800

    spring cloud 使用

--- lwh-springcloud-2020/cloud-consumer-order80/src/main/java/com/lwhtarena/springcloud/controller/OrderController.java
+++ lwh-springcloud-2020/cloud-consumer-order80/src/main/java/com/lwhtarena/springcloud/controller/OrderController.java
@@ -4,9 +4,7 @@ import com.lwhtarena.springcloud.entities.CommonResult;
 import com.lwhtarena.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -25,8 +23,8 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment){
+    @PostMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(@RequestBody Payment payment){
         //写操作
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
     }
