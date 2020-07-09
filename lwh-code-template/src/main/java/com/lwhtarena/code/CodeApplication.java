a6101fec66644dbca739d77e961b2445273f475e
liwh <lwhtarena@163.com>
2020-07-09 11:13:40 +0800

    模板生成器

---
+++ lwh-code-template/src/main/java/com/lwhtarena/code/CodeApplication.java
@@ -0,0 +1,18 @@
+package com.lwhtarena.code;
+
+import com.lwhtarena.code.build.TemplateBuilder;
+
+/**
+ * @author liwh
+ * @Title: CodeApplication
+ * @Package com.lwhtarena.code
+ * @Description:
+ * @Version 1.0.0
+ * @date 2020/7/9 11:06
+ */
+public class CodeApplication {
+    public static void main(String[] args) {
+        //调用该方法即可
+        TemplateBuilder.builder();
+    }
+}
